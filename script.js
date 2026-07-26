c11bebd4e58ec0c6673ff3389a737a02906a78e0
RafaelJPEG <188954702+RafaelJPEG@users.noreply.github.com>
2026-07-27 07:55:10 +0800

    modify/reaname multipleBox function into createBoxBatch. This function now creates batches of boxes. ps(other files is just trial error for code)

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
 const squareContainer = document.querySelector(".squareContainer")
+squareContainer.setAttribute(`style`, `display: flex`)
 
-function createSquare() {
+function createSquare(miniSquareContainer) {
+    // This function will create div box.
     const square = document.createElement("div")
-    square.setAttribute(`style`, `border: 2px solid black; width: 50px; height: 50px;`);
-    squareContainer.appendChild(square);
+    square.setAttribute(`style`, `border: 1px solid green; width: 50px; height: 50px; flexGrow: 1;`);
+    miniSquareContainer.appendChild(square);
+    
     
     square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "red";
     });
 
+    return square;
+
 };
 
-function multiplySqaure(multiplyBy) {
-    let times = multiplyBy
-    for (let i = 0; i < times; i++) {
-        createSquare()
+const userInput = document.getElementById("btn1");
+
+userInput.addEventListener("click", () => {
+
+    // This event listner will get user input (number) and multiply it.
+ const num = prompt("Input number of squares per side: (max 100)")
+
+    if (num <= 100 && num >= 1) {
+    
+        for (let i = 0; i < num; i++) {
+            squareContainer.appendChild(createBoxBatch(num));
+        };
+
+    } else {
+        alert("Invalid input!")
     };
 
+});
+
+function createBoxBatch(times) {
+
+    const boxBatch = document.createElement("div")
+    // boxBatch.setAttribute(`style`, `display: flex;`)
+    
+
+    for (let i = 0; i < times; i++) {
+        boxBatch.appendChild(createSquare(boxBatch));
+    };
+
+    return boxBatch
+    console.log(boxBatch)
 };
-
-multiplySqaure(256);
-
-
-
