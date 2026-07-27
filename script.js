34169ba1f986266ef6e081c97b203c234be04e26
RafaelJPEG <188954702+RafaelJPEG@users.noreply.github.com>
2026-07-27 10:32:27 +0800

    add if statements for userInput (button) event listner. This will remove current child elements of squareContainer, then add a new one.

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const squareContainer = document.querySelector(".squareContainer")
 squareContainer.setAttribute(`style`, `display: flex; flexGrow: 1; flex-wrap: wrap;`)
 
 function createSquare(miniSquareContainer) {
+    
     // This function will create div box.
+    
     const square = document.createElement("div")
     square.setAttribute(`style`, `border: 1px solid green; width: 50px; height: auto; flex-grow: 1;`);
     miniSquareContainer.appendChild(square);
-    
-    
+        
     square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "red";
     });
@@ -20,11 +21,19 @@ const userInput = document.getElementById("btn1");
 
 userInput.addEventListener("click", () => {
 
-    // This event listner will get user input (number) and multiply it.
-    //  const num = prompt("Input number of squares per side: (max 100)")
-    const num = 100;
+    // This event listner will get user input (number) and use it as the number of box each side.
+    
+    const num = prompt("Input number of squares per side: (max 100)")
 
     if (num <= 100 && num >= 1) {
+        
+        if (squareContainer.innerHTML) {
+        
+            // this code block will remove square containers content if there is,
+            // and when its parent (button) was cliked.
+            
+            squareContainer.innerHTML = '';
+        };
     
         for (let i = 0; i < num; i++) {
             squareContainer.appendChild(createBoxBatch(num));
@@ -33,15 +42,15 @@ userInput.addEventListener("click", () => {
     } else {
         alert("Invalid input!")
     };
-
 });
 
 function createBoxBatch(times) {
 
+    // This function will create a container for a boxes. The saidcontainer will then append to main box (square container).
+
     const boxBatch = document.createElement("div")
     boxBatch.setAttribute(`style`, `display: flex; flexGrow: 1; height: auto; width: 100%;`)
     
-
     for (let i = 0; i < times; i++) {
         boxBatch.appendChild(createSquare(boxBatch));
     };
